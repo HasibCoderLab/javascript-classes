6cec3a537fcfc53a98203a56cfa09bf385ac405c
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-26 20:34:12 +0600

    comment out old  code

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // ===================|     Example 01    |==================
-class ToyotaCars {
-    start(){
-        console.log("Run The Car");
+// class ToyotaCars {
+//     start(){
+//         console.log("Run The Car");
+        
+//     }
+
+//     Stop(){
+//         console.log("Break The Car");
+        
+//     }
+//     setBrand(brand){
+//         this.BradVar = brand 
+//     }
+
+// }
+
+// let fortuner = new ToyotaCars();
+// fortuner.setBrand("Fortuner");
+// let lexus = new ToyotaCars();
+// lexus.setBrand("Lexus");
+// ===================|   *constructor *    |===========================
+class name {
+    constructor(parameters) {
         
     }
-
-    Stop(){
-        console.log("Break The Car");
-        
-    }
-    setBrand(brand){
-        this.BradVar = brand 
-    }
-
 }
-
-let fortuner = new ToyotaCars();
-fortuner.setBrand("Fortuner");
-let lexus = new ToyotaCars();
-lexus.setBrand("Lexus");
